d5338e2492b73ced2d4705fd964079859a970369
Amanda King <amapickell@gmail.com>
2020-05-12 21:14:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,41 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
     
+    DB[:conn].execute(sql)
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.first_X_students_in_grade_10 (number)
+    sql = <<-SQL
+    
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.all_students_in_grade_X (grade)
+    sql = <<-SQL
+    
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def save
