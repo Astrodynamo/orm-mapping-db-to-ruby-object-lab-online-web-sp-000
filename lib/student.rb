c84d564eaa602d51083ea749ac65cd84e773ae0a
Amanda King <amapickell@gmail.com>
2020-05-12 21:24:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,9 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end.first
   end
   
   def self.all_students_in_grade_X (grade)
